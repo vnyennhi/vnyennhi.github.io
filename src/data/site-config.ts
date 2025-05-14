c9feed44d1c52faf6065004e5a851ba0cf1e4d48
Nhi Vo <vnyennhi@gmail.com>
2025-05-14 14:46:11 +1000

    Update site-config.ts

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -97,7 +97,7 @@ const siteConfig: SiteConfig = {
     ],
     hero: {
         title: '',
-        text: "I'm **Nhi Vo**, a data science and analytics leader with a Ph.D. and a deep love for all things data — especially the messy, complicated kind. I thrive on optimizing algorithms, designing scalable analytics systems, and making sense of chaos with machine learning and statistical models. My expertise spans machine learning, statistical modeling, and large-scale data analysis, enabling teams to make informed, data-driven decisions. I have a passion for sharing knowledge and insights, and I believe in the power of collaboration to drive innovation. Feel free to reach out if you want to connect or collaborate.",
+        text: "I'm **Nhi Vo**, a data science and analytics leader with a Ph.D. and a deep love for all things data — especially the messy, complicated kind. I thrive on building pipelines, optimizing algorithms, designing scalable analytics systems, and making sense of chaos with machine learning and statistical models. My expertise spans machine learning, statistical modeling, and large-scale data analysis, enabling teams to make informed, data-driven decisions. I have a passion for sharing knowledge and insights, and I believe in the power of collaboration to drive innovation. Feel free to reach out if you want to connect or collaborate.",
         image: {
             src: '/profile.jpg',
             alt: 'My carricature',
